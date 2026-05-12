0fb54d4d9ca063af7b88800f083afff36bfd7219
Henriva17 <henritresor405@gmail.com>
2026-05-13 00:43:49 +0200

    Add production environment with Render URL

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  url: 'https://mmp-ztuk.onrender.com'  // your cloud Spring Boot
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-    url:'https://your-mmp-backend.onrender.com/api'
+    url: 'http://localhost:8080/api/'
   };
